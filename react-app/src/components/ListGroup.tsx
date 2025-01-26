cd91d656125d94334221cfdece67908f17e1c1c0
Joel Marriott <joel.marriott.uk@gmail.com>
2025-01-26 20:05:11 +0000

    Condition reformatting

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,5 +1,5 @@
 function ListGroup() {
-  const items = [
+  let items = [
     "New York",
     "San Francisco",
     "Tokyo",
@@ -7,10 +7,12 @@ function ListGroup() {
     "Paris",
     "Bristol",
   ];
+  items = [];
 
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
